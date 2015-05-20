2c6d0e9c03e8af5baad2c68bacbdbb25da8e954c
aadmiqdad <aadmiqdad@gmail.com>
2015-05-20 08:37:43 +0700

    aku tambahkan function untuk CRUD service

--- application/controllers/kamar.php
+++ application/controllers/kamar.php
@@ -13,4 +13,31 @@ class Kamar extends CI_Controller {
 		$kamar = $this->kamar_m->selectAll();
 		echo  json_encode($kamar);
 	}
+
+	function getBy()
+	{
+		$id = $this->uri->segment(3);
+		$kamar = $this->kamar_m->selectBy('kd_kamar', $id);
+		echo  json_encode($kamar);
+	}
+
+	function postInsert()
+	{
+		$data = json_decode(file_get_contents("php://input"));
+        echo  $this->kamar_m->insert($data);
+	}
+
+	function postUpdate()
+	{
+		$id = $this->uri->segment(3);
+		$data = json_decode(file_get_contents("php://input"));
+        echo  $this->kamar_m->update($data, $id);
+	}
+
+	function delete()
+	{
+		$id = $this->uri->segment(3);
+        echo  $this->kamar_m->delete($id);
+	}
+
 }
