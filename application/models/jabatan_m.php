a3abb181f16dbbf5d11128f6024729a98a4cb1af
rizakbar2 <rizaelektro12@gmail.com>
2015-05-26 09:49:06 +0700

    model v.1.0.0

---
+++ application/models/jabatan_m.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Jabatan_m extends CI_Model
+{
+
+  public $table = "tb_jabatan";
+  public $id = "kd_jabatan";
+
+  function selectAll()
+  {
+    $query = $this->db->get($this->table);
+    if ($query->num_rows()>0)
+    {
+      return $query->result();
+    }
+    else
+    {
+      return array();
+    }
+  }
+
+  function selectBy($field, $data)
+  {
+    $this->db->where($field, $data);
+    $query = $this->db->get($this->table);
+    return $query->result();
+  }
+
+  function insert($data)
+  {
+    $query = $this->db->insert($this->table, $data);
+    return $query;
+  }
+
+  function update($data, $id)
+  {
+    $this->db->where($this->id, $id);
+    $query = $this->db->update($this->table, $data);
+    return $query;
+  }
+
+  function delete($id)
+  {
+    $this->db->where($this->id, $id);
+    $query = $this->db->delete($this->table);
+    return $query;
+  }
+
+}
